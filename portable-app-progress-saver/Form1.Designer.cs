b39aa559ca5d90121bd5254e1a3e9f8a4da51f9f
BuildTools <unconfigured@null.spigotmc.org>
2023-10-18 15:17:46 -0500

    ui design+ tf2 image

--- portable-app-progress-saver/Form1.Designer.cs
+++ portable-app-progress-saver/Form1.Designer.cs
@@ -28,13 +28,149 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.apppathlabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.appdatalabel = new System.Windows.Forms.Label();
+            this.exepath = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.appdatapath = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(97, 95);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 113);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(665, 290);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.appdatapath);
+            this.panel1.Controls.Add(this.exepath);
+            this.panel1.Controls.Add(this.appdatalabel);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.apppathlabel);
+            this.panel1.Location = new System.Drawing.Point(115, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(673, 95);
+            this.panel1.TabIndex = 2;
+            // 
+            // apppathlabel
+            // 
+            this.apppathlabel.AutoSize = true;
+            this.apppathlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.apppathlabel.Location = new System.Drawing.Point(4, 16);
+            this.apppathlabel.Name = "apppathlabel";
+            this.apppathlabel.Size = new System.Drawing.Size(96, 20);
+            this.apppathlabel.TabIndex = 3;
+            this.apppathlabel.Text = "Executable:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 16);
+            this.label2.TabIndex = 3;
+            // 
+            // appdatalabel
+            // 
+            this.appdatalabel.AutoSize = true;
+            this.appdatalabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.appdatalabel.Location = new System.Drawing.Point(4, 47);
+            this.appdatalabel.Name = "appdatalabel";
+            this.appdatalabel.Size = new System.Drawing.Size(123, 20);
+            this.appdatalabel.TabIndex = 3;
+            this.appdatalabel.Text = "App Data Path:";
+            // 
+            // exepath
+            // 
+            this.exepath.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.exepath.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exepath.Location = new System.Drawing.Point(106, 6);
+            this.exepath.Name = "exepath";
+            this.exepath.Size = new System.Drawing.Size(476, 31);
+            this.exepath.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(583, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 31);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // appdatapath
+            // 
+            this.appdatapath.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.appdatapath.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.appdatapath.Location = new System.Drawing.Point(132, 43);
+            this.appdatapath.Name = "appdatapath";
+            this.appdatapath.Size = new System.Drawing.Size(450, 31);
+            this.appdatapath.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(583, 43);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 31);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Browse";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label appdatalabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label apppathlabel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox exepath;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox appdatapath;
     }
 }
 
--- portable-app-progress-saver/Form1.cs
+++ portable-app-progress-saver/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +16,22 @@ namespace portable_app_progress_saver
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+
+                pictureBox1.BackgroundImageLayout = ImageLayout.Zoom;
+                pictureBox1.BackgroundImage = Bitmap.FromStream(WebRequest.Create("https://cdn.discordapp.com/attachments/1104813377836036237/1164212783307571350/IMG_1945.jpg").GetResponse().GetResponseStream());
+            }
+            catch 
+            {
+                pictureBox1.Hide();
+                panel1.Location = pictureBox1.Location;
+            }
+            
         }
     }
 }
